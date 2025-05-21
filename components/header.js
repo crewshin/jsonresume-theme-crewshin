3b9f28473e2fd520ca9a3e2e7e6cfba7eb60d5e3
Gene Crucean <genecrucean@gmail.com>
2025-05-21 13:14:35 -0500

    Test version update

--- components/header.js
+++ components/header.js
@@ -1,14 +1,14 @@
-import { html } from '@rbardini/html'
-import markdown from '../utils/markdown.js'
-import Icon from './icon.js'
-import Link from './link.js'
+import { html } from "@rbardini/html"
+import markdown from "../utils/markdown.js"
+import Icon from "./icon.js"
+import Link from "./link.js"
 
 /**
  * @param {string} countryCode
  * @returns {string | undefined}
  */
 const formatCountry = countryCode =>
-  Intl.DisplayNames ? new Intl.DisplayNames(['en'], { type: 'region' }).of(countryCode) : countryCode
+  Intl.DisplayNames ? new Intl.DisplayNames(["en"], { type: "region" }).of(countryCode) : countryCode
 
 /**
  * @param {import('../schema.d.ts').ResumeSchema['basics']} basics
@@ -26,32 +26,33 @@ export default function Header(basics = {}) {
         ${location?.city &&
         html`
           <li>
-            ${Icon('map-pin')} ${location.city}${location.countryCode && html`, ${formatCountry(location.countryCode)}`}
+            ${Icon("map-pin")} ${location.city}${location.countryCode && html`, ${formatCountry(location.countryCode)}`}
           </li>
         `}
         ${email &&
         html`
           <li>
-            ${Icon('mail')}
+            ${Icon("mail")}
             <a href="mailto:${email}">${email}</a>
           </li>
         `}
         ${phone &&
         html`
           <li>
-            ${Icon('phone')}
-            <a href="tel:${phone.replace(/\s/g, '')}">${phone}</a>
+            ${Icon("phone")}
+            <a href="tel:${phone.replace(/\s/g, "")}">${phone}</a>
           </li>
         `}
-        ${url && html`<li>${Icon('link')} ${Link(url)}</li>`}
+        ${url && html`<li>${Icon("link")} ${Link(url)}</li>`}
         ${profiles.map(
           ({ network, url, username }) => html`
             <li>
-              ${network && Icon(network, 'user')} ${Link(url, username)}
+              ${network && Icon(network, "user")} ${Link(url, username)}
               ${network && html`<span class="network">(${network})</span>`}
             </li>
           `,
         )}
+        <li>Test</li>
       </ul>
     </header>
   `
